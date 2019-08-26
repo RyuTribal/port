507d9f6121036e4caae7370402247e98b93e9408
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-08-26 09:27:06 +0200

    Added scroll animations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import "./App.scss";
 import "./js/main.js";
 import "./js/canvas.js";
 import "./js/scroll.js";
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
+import Bounce from "react-reveal/Bounce";
+import Flip from "react-reveal/Flip";
 import imgLeft from "./img/ivan-img-left.png";
 import imgRight from "./img/ivan-img-right.png";
 import notey from "./img/notey.PNG";
@@ -17,11 +21,6 @@ import cLang from "./img/c++.png";
 import node from "./img/node.png";
 import reactImg from "./img/react.png";
 import javascript from "./img/javascript.png";
-import clientIdentification from "./clientId.json";
-
-var clientId = clientIdentification.id;
-var apiKey = clientIdentification.key;
-var scopes = "https://www.googleapis.com/auth/gmail.readonly";
 
 class App extends Component {
   render() {
@@ -54,22 +53,30 @@ class App extends Component {
               <a href="#contact" className="nav-mobile-options">
                 Contact
               </a>
+              <a href="#cv" className="nav-mobile-options">
+                CV
+              </a>
             </div>
           </div>
-          <div className="nav-options-container">
-            <a href="#about" className="nav-options">
-              About
-            </a>
-            <a href="#exp" className="nav-options">
-              Experience
-            </a>
-            <a href="#skills" className="nav-options">
-              Skills
-            </a>
-            <a href="#contact" className="nav-options">
-              Contact
-            </a>
-          </div>
+          <Bounce right>
+            <div className="nav-options-container">
+              <a href="#about" className="nav-options">
+                About
+              </a>
+              <a href="#exp" className="nav-options">
+                Experience
+              </a>
+              <a href="#skills" className="nav-options">
+                Skills
+              </a>
+              <a href="#contact" className="nav-options">
+                Contact
+              </a>
+              <a href="#cv" className="nav-options">
+                CV
+              </a>
+            </div>
+          </Bounce>
         </nav>
         <section className="page intro">
           <div className="intro-content">
@@ -86,154 +93,190 @@ class App extends Component {
         <section id="about" className="non-intro page about">
           <h2 className="section-title">About</h2>
           <div className="row about-container">
-            <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 padding">
-              <img className="personal-img" src={imgLeft} />
-            </div>
-            <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 padding">
-              <img className="personal-img" src={imgRight} />
-            </div>
-
-            <div className="paragraph-container col-xs-12 col-sm-12 col-md-4 col-lg-6 padding">
-              <p>
-                Hey.
-                <br /> My name is Ivan Sedelkin and I'm a front-end and back-end
-                developer with a love for cutting edge technolodgy that drives
-                me to try and stay up to date with the newest tech out there.
-              </p>
-              <p>
-                My dream is to work for Square Enix and create games that can
-                inspire kids the same way Square's games inpired me down this
-                path so many year ago.
-              </p>
-              <p>
-                I believe web applications and games alike share similarities
-                and should be studied together in order to create some of the
-                prettiest systems that can branch of from the norm. That is the
-                reason I like developing webbapplikations as well.
-              </p>
-              <p>
-                On my sparetime I like keeping my body active by going to the
-                gym and playing basketball. My love for basketball has lead me
-                to pursue coaching on the side for kids age 13-14. Books aren't
-                half bad either.
-              </p>
-            </div>
+            <Fade left>
+              <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 padding">
+                <img className="personal-img" src={imgLeft} />
+              </div>
+            </Fade>
+            <Fade bottom>
+              <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 padding">
+                <img className="personal-img" src={imgRight} />
+              </div>
+            </Fade>
+            <Fade right>
+              <div className="paragraph-container col-xs-12 col-sm-12 col-md-4 col-lg-6 padding">
+                <p>
+                  Hey.
+                  <br /> My name is Ivan Sedelkin and I'm a front-end and
+                  back-end developer with a love for cutting edge technolodgy
+                  that drives me to try and stay up to date with the newest tech
+                  out there.
+                </p>
+                <p>
+                  My dream is to work for Square Enix and create games that can
+                  inspire kids the same way Square's games inpired me down this
+                  path so many year ago.
+                </p>
+                <p>
+                  I believe web applications and games alike share similarities
+                  and should be studied together in order to create some of the
+                  prettiest systems that can branch of from the norm. That is
+                  the reason I like developing webbapplikations as well.
+                </p>
+                <p>
+                  On my sparetime I like keeping my body active by going to the
+                  gym and playing basketball. My love for basketball has lead me
+                  to pursue coaching on the side for kids age 13-14. Books
+                  aren't half bad either.
+                </p>
+              </div>
+            </Fade>
           </div>
         </section>
         <section id="exp" className="non-intro page exp">
           <h2 className="section-title">Experience</h2>
-          <div className="row exp-container">
-            <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
-              <figure>
-                <img className="exp-img" src={notey} alt="" />
-                <figcaption className="fig-captions">
-                  <div className="overlay">
-                    <h5>
-                      <a href="http://finalnotey.herokuapp.com/start">Notey</a>
-                      <br />
-                      School project
-                      <br />
-                      Front-end and Back-end
-                    </h5>
-                  </div>
-                </figcaption>
-              </figure>
+          <Fade bottom>
+            <div className="row exp-container">
+              <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
+                <figure>
+                  <img className="exp-img" src={notey} alt="" />
+                  <figcaption className="fig-captions">
+                    <div className="overlay">
+                      <h5>
+                        <a href="http://finalnotey.herokuapp.com/start">
+                          Notey
+                        </a>
+                        <br />
+                        School project
+                        <br />
+                        Front-end and Back-end
+                      </h5>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
+              <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
+                <figure>
+                  <img className="exp-img" src={itjobs} alt="" />
+                  <figcaption className="fig-captions">
+                    <div className="overlay">
+                      <h5>
+                        <a href="https://itjobsfinal.herokuapp.com/">
+                          IT-jobs-finder
+                        </a>
+                        <br />
+                        School project
+                        <br />
+                        Front-end and Back-end
+                      </h5>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
             </div>
-            <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
-              <figure>
-                <img className="exp-img" src={itjobs} alt="" />
-                <figcaption className="fig-captions">
-                  <div className="overlay">
-                    <h5>
-                      <a href="https://itjobsfinal.herokuapp.com/">
-                        IT-jobs-finder
-                      </a>
-                      <br />
-                      School project
-                      <br />
-                      Front-end and Back-end
-                    </h5>
-                  </div>
-                </figcaption>
-              </figure>
+          </Fade>
+          <Fade bottom>
+            <div className="row exp-container">
+              <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
+                <figure>
+                  <img className="exp-img" src={huddinge} alt="" />
+                  <figcaption className="fig-captions">
+                    <div className="overlay">
+                      <h5>
+                        <a href="http://www.huddingebasket.se/t">
+                          Huddinge Basketball
+                        </a>
+                        <br />
+                        Coach/Front-end/back-end
+                        <br />
+                        Stockholm
+                      </h5>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
+              <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
+                <figure>
+                  <img className="exp-img" src={justvalue} alt="" />
+                  <figcaption className="fig-captions">
+                    <div className="overlay">
+                      <h5>
+                        <a href="http://justvalue.se/">Just value</a>
+                        <br />
+                        Front-end/back-end(internship)
+                        <br />
+                        Stockholm
+                      </h5>
+                    </div>
+                  </figcaption>
+                </figure>
+              </div>
             </div>
-          </div>
-          <div className="row exp-container">
-            <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
-              <figure>
-                <img className="exp-img" src={huddinge} alt="" />
-                <figcaption className="fig-captions">
-                  <div className="overlay">
-                    <h5>
-                      <a href="http://www.huddingebasket.se/t">
-                        Huddinge Basketball
-                      </a>
-                      <br />
-                      Coach/Front-end/back-end
-                      <br />
-                      Stockholm
-                    </h5>
-                  </div>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="exp-link col-xs-12 col-sm-12 col-md-12 col-lg-6 img-grid">
-              <figure>
-                <img className="exp-img" src={justvalue} alt="" />
-                <figcaption className="fig-captions">
-                  <div className="overlay">
-                    <h5>
-                      <a href="http://justvalue.se/">Just value</a>
-                      <br />
-                      Front-end/back-end(internship)
-                      <br />
-                      Stockholm
-                    </h5>
-                  </div>
-                </figcaption>
-              </figure>
-            </div>
-          </div>
+          </Fade>
         </section>
         <section id="skills" className="non-intro page skills">
           <h2 className="section-title">Skills</h2>
           <p>My most used languages, tools and frameworks</p>
           <div className="skills-container row">
             <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
-              <div className="fab-img">
-                <img src={javascript}></img>
-              </div>
-              <h3>Javascript</h3>
+              <Zoom>
+                <div className="fab-img">
+                  <img src={javascript}></img>
+                </div>
+              </Zoom>
+              <Fade bottom>
+                <h3>Javascript</h3>
+              </Fade>
             </div>
             <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
-              <div className="fab-img">
-                <img src={cLang}></img>
-              </div>
-              <h3>C++</h3>
+              <Zoom>
+                <div className="fab-img">
+                  <img src={cLang}></img>
+                </div>
+              </Zoom>
+              <Fade bottom>
+                <h3>C++</h3>
+              </Fade>
             </div>
             <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
-              <div className="fab-img">
-                <img src={node}></img>
-              </div>
-              <h3>nodejs</h3>
+              <Zoom>
+                <div className="fab-img">
+                  <img src={node}></img>
+                </div>
+              </Zoom>
+              <Fade bottom>
+                <h3>nodejs</h3>
+              </Fade>
             </div>
             <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
-              <div className="fab-img">
-                <img src={unreal}></img>
-              </div>
-              <h3>Unreal Engine</h3>
+              <Zoom>
+                <div className="fab-img">
+                  <img src={unreal}></img>
+                </div>
+              </Zoom>
+              <Fade bottom>
+                <h3>Unreal Engine</h3>
+              </Fade>
             </div>
             <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
-              <div className="fab-img">
-                <img src={maya}></img>
-              </div>
-              <h3>Maya/Mudbox-combo</h3>
+              <Zoom>
+                <div className="fab-img">
+                  <img src={maya}></img>
+                </div>
+              </Zoom>
+              <Fade bottom>
+                <h3>Maya/Mudbox-combo</h3>
+              </Fade>
             </div>
             <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
-              <div className="fab-img">
-                <img src={reactImg}></img>
-              </div>
-              <h3>React</h3>
+              <Zoom>
+                <div className="fab-img">
+                  <img src={reactImg}></img>
+                </div>
+              </Zoom>
+              <Fade bottom>
+                <h3>React</h3>
+              </Fade>
             </div>
           </div>
         </section>
@@ -243,21 +286,27 @@ class App extends Component {
             <div className="skills-container row">
               <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
                 <i class="fas fa-envelope"></i>
-                <h3>Ivan.sedelkin9@gmail.com</h3>
+                <Fade bottom>
+                  <h3>Ivan.sedelkin9@gmail.com</h3>
+                </Fade>
               </div>
               <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
                 <i class="fab fa-linkedin-in"></i>
-                <h3>
-                  <a href="https://www.linkedin.com/in/ivan-sedelkin-236068191">
-                    LinkedIn
-                  </a>
-                </h3>
+                <Fade bottom>
+                  <h3>
+                    <a href="https://www.linkedin.com/in/ivan-sedelkin-236068191">
+                      LinkedIn
+                    </a>
+                  </h3>
+                </Fade>
               </div>
               <div className="skill col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-v align-center">
                 <i class="fab fa-github"></i>
-                <h3>
-                  <a href="https://github.com/RyuTribal">Github</a>
-                </h3>
+                <Fade bottom>
+                  <h3>
+                    <a href="https://github.com/RyuTribal">Github</a>
+                  </h3>
+                </Fade>
               </div>
             </div>
             <h2 className="section-title">Requests:</h2>
